f764b2d5138e5595eb67d5a2f93245a28e556681
tabbathacrouch <tabbathacrouch@gmail.com>
2021-02-22 11:35:15 -0600

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
         .then(response => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://secret-hamlet-67600.herokuapp.com:3000/image", {
+          fetch("https://secret-hamlet-67600.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
