10139f470142403fe0efe15dd764d2c558ebc1d7
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-07-21 15:59:49 -0500

    add mode to sigin fetch

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     event.preventDefault();
     fetch("https://secret-hamlet-67600.herokuapp.com/signin", {
       method: "post",
-      headers: { "content-type": "application/json" },
+      mode: "no-cors",
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
